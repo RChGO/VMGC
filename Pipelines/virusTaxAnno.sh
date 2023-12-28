fa279e30883f6d315df470ae7ef759e835b0c1db
RChGO <45479925+RChGO@users.noreply.github.com>
2023-12-28 11:47:10 +0800

    Update virusTaxAnno.sh

--- Pipelines/virusTaxAnno.sh
+++ Pipelines/virusTaxAnno.sh
@@ -38,9 +38,9 @@ msort -k 1,rn3,rn6 $outf.pep.bt2.f2 | perl -ne 'chomp;@s=split /\s+/;next if exi
 blastn -db $db_s -query $geno -out $outf.nucl.bt -outfmt 6 -evalue 1e-5 -num_threads $threads -max_target_seqs 50 -word_size 28
 /share/data2/guorc/script/fasta_length -i $geno -o $outf.nucl.len
 /share/data1/zhangy2/scripts/blast_cvg.v1.py $outf.nucl.bt $outf.nucl.cvg 0 200
-/share/data2/guorc/script/TableTreat.v4.py -a $outf.nucl.cvg -b $outf.nucl.len -m table_merge -o $outf.nucl.cvg
+/share/data2/guorc/script/TableTreat.py -a $outf.nucl.cvg -b $outf.nucl.len -m table_merge -o $outf.nucl.cvg
 cat $outf.nucl.cvg | perl -ne '@a=split/\t/; $cov=$a[2]/$a[6]*100; if($cov > 75 && $a[3] > 95){$ord=$a[3]*$cov; print "$a[0]\t$a[1]\t$a[3]\t$cov\t$ord\n"}' | msort -t '\t' -k 1,rn5 | perl -ne '@s=split /\s+/;print "$s[0]\t$s[1]\t$s[2]\t$s[3]\n" unless $a eq $s[0];$a=$s[0];' > $outf.nucl.cvg2
-/share/data2/guorc/script/TableTreat.v4.py -a $outf.nucl.cvg2 -b $db_s_tax -o $outf.nucl.cvg2 -x 1 -y 1  -m table_merge
+/share/data2/guorc/script/TableTreat.py -a $outf.nucl.cvg2 -b $db_s_tax -o $outf.nucl.cvg2 -x 1 -y 1  -m table_merge
 cat $outf.nucl.cvg2 | perl -ne '@a = split(/\t/,$_); @b=/;f_(.*?);/ ; @c=/;s_(.*?)$/; print "$a[0]\t$b[0]\t$c[0]\n"' | perl -pne 's/\t\t/\tUnclassified\t/' | sed -e '1i ID\tfamily\tspecies' -e  's/ /_/g' > $outf.tax_species
 
 
